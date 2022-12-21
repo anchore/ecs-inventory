c381bc413ac7bd9c32849ddc3fb5acb09c25f19a
Bradley Jones <bradley.jones@anchore.com>
2022-12-21 18:15:39 +0000

    fix: ensure Report reponse object matches current Anchore API
    Even though namespace/clustername may not be accurate descriptions for
    ECS based workflow we are overloading those terms here to get something
    working in Enterprise without API changes.
    
    Signed-off-by: Bradley Jones <bradley.jones@anchore.com>

--- ecg/inventory/reportitem.go
+++ ecg/inventory/reportitem.go
@@ -6,8 +6,8 @@ import (
 
 // ReportItem represents a cluster and all it's unique images
 type ReportItem struct {
-	ClusterARN string        `json:"cluster_arn,omitempty"`
-	Images     []ReportImage `json:"images"`
+	Namespace string        `json:"namespace,omitempty"` //NOTE The key is Namespace to match the Anchore API but it's actually the cluster ARN
+	Images    []ReportImage `json:"images"`
 }
 
 // ReportImage represents a unique image in a cluster
@@ -18,7 +18,7 @@ type ReportImage struct {
 
 // String represent the ReportItem as a string
 func (r *ReportItem) String() string {
-	return fmt.Sprintf("ReportItem(cluster=%s, images=%v)", r.ClusterARN, r.Images)
+	return fmt.Sprintf("ReportItem(cluster=%s, images=%v)", r.Namespace, r.Images)
 }
 
 // key will return a unique key for a ReportImage
